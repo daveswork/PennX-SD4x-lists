ff918c66836cd7608a2f1fd1b4cdcbdcde9e3615
Dave J <daveswork@users.noreply.github.com>
2017-09-06 17:32:39 -0400

    Completed Part 3. Marking a ListItem

--- src/ListItem.js
+++ src/ListItem.js
@@ -4,19 +4,24 @@ class ListItem extends Component {
 
     constructor(props) {
 	super(props);
-	this.state = { color: 'black' };
+	this.state = { //color: 'black',
+                   amDone: false 
+        };
     }
 
     handleClick() {
 	// Implement this function!
+	this.setState({amDone: !this.state.amDone});
     }
 
   render() {
     var item = this.props.item;
     var name = item.name;
+    var iColor = this.state.amDone?'grey':'black';
+    //console.log(cToggle);
 
     return (
-	    <span onClick={this.handleClick.bind(this)} style={{color: this.state.color}}>
+	    <span onClick={this.handleClick.bind(this)} style={{color: iColor}}>
         <strong>{name}</strong>
       </span>
     );
